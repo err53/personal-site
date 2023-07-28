af25a8b8a8b88c3c16728121393c345660a40490
Jason Huang <jasonhuang20035@gmail.com>
2023-07-28 15:41:36 +0000

    tear down vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/static";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   experimental: {
     assets: true,
   },
-  output: "static",
-  adapter: vercel({
-    analytics: true,
-    imagesConfig: {
-      sizes: [320, 640, 1280, 2560],
-    },
-    imageService: true,
-  }),
 });

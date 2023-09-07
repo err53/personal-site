1f56d7020bc64d88ee038a60759cc944c43561aa
Jason Huang <jasonhuang20035@gmail.com>
2023-09-07 17:56:50 -0400

    migrate to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+    imageService: true,
+    imagesConfig: {
+      sizes: [398, 796, 1360],
+    },
+  })
 });

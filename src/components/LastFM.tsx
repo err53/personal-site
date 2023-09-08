969337285c3d82b0af7386276b6d8a0e9b9c882f
Jason Huang <jasonhuang20035@gmail.com>
2023-09-08 13:35:29 -0400

    Add less jank transitions

--- src/components/LastFM.tsx
+++ src/components/LastFM.tsx
@@ -1,4 +1,5 @@
 import { useLastFM } from "use-last-fm";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export const CurrentlyPlaying = () => {
   const lastFM = useLastFM(
@@ -8,21 +9,25 @@ export const CurrentlyPlaying = () => {
     "medium"
   );
 
-  if (lastFM.status !== "playing") {
-    return (
-      <div className="flex h-full flex-row border shadow-md group-hover:bg-slate-50 group-active:bg-slate-100 group-active:shadow-none">
-        <p className="p-4 my-auto">Not listening to anything...</p>
-      </div>
-    );
-  }
+  const [parent] = useAutoAnimate();
 
   return (
-    <a href={lastFM.song.url}>
-      <div className="flex h-full flex-row border shadow-md group-hover:bg-slate-50 group-active:bg-slate-100 group-active:shadow-none">
-        <img src={lastFM.song.art} className="my-auto lg:w-20" />
-        <p className="px-4 my-auto">
-          Listening to <i>{lastFM.song.name}</i> by <i>{lastFM.song.artist}</i>
-        </p>
+    <a href={lastFM.status == "playing" ? lastFM.song.url : undefined}>
+      <div
+        ref={parent}
+        className="flex h-full flex-row border shadow-md group-hover:bg-slate-50 group-active:bg-slate-100 group-active:shadow-none"
+      >
+        {lastFM.status == "playing" && (
+          <img src={lastFM.song.art} className="my-auto lg:w-20" />
+        )}
+        {lastFM.status == "playing" ? (
+          <p className="px-4 py-2 my-auto">
+            Listening to <i>{lastFM.song.name}</i> by{" "}
+            <i>{lastFM.song.artist}</i>
+          </p>
+        ) : (
+          <p className="px-4 py-2 my-auto">Not listening to anything...</p>
+        )}
       </div>
     </a>
   );

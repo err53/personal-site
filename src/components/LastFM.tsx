6185032515a6a55658b93e71ca08eb1e7d0195b5
Jason Huang <jasonhuang20035@gmail.com>
2023-09-08 13:01:07 -0400

    Revert "Add jank transitions"
    This reverts commit 3ecda5400907daf307505a174196eea09e4fd3b0.

--- src/components/LastFM.tsx
+++ src/components/LastFM.tsx
@@ -1,5 +1,4 @@
 import { useLastFM } from "use-last-fm";
-import TinyCrossfade from "react-tiny-crossfade";
 
 export const CurrentlyPlaying = () => {
   const lastFM = useLastFM(
@@ -9,37 +8,22 @@ export const CurrentlyPlaying = () => {
     "medium"
   );
 
+  if (lastFM.status !== "playing") {
+    return (
+      <div className="flex h-full flex-row border shadow-md group-hover:bg-slate-50 group-active:bg-slate-100 group-active:shadow-none">
+        <p className="p-4 my-auto">Not listening to anything...</p>
+      </div>
+    );
+  }
+
   return (
-    <a href={lastFM?.song?.url || ""}>
-      <TinyCrossfade
-        classNames={{
-          beforeEnter: "opacity-0",
-          entering: "opacity-1 transition-opacity duration-500",
-          beforeLeave: "opacity-1",
-          leaving: "opacity-0 transition-opacity duration-500",
-        }}
-        className="flex h-full border shadow-md hover:bg-slate-50 active:bg-slate-100 active:shadow-none transition-height duration-500 overflow-hidden"
-      >
-        {lastFM.status !== "playing" ? (
-          <p key="notplaying" className="p-4 my-auto">
-            Not listening to anything...
-          </p>
-        ) : (
-          <div key={lastFM.song.url} className="flex flex-row">
-            <img
-              src={lastFM.song.art}
-              alt={`The album art for ${lastFM.song.album}`}
-              width="64px"
-              height="64px"
-              className="my-auto lg:w-20"
-            />
-            <p className="px-4 my-auto">
-              Listening to <i>{lastFM.song.name}</i> by{" "}
-              <i>{lastFM.song.artist}</i>
-            </p>
-          </div>
-        )}
-      </TinyCrossfade>
+    <a href={lastFM.song.url}>
+      <div className="flex h-full flex-row border shadow-md group-hover:bg-slate-50 group-active:bg-slate-100 group-active:shadow-none">
+        <img src={lastFM.song.art} className="my-auto lg:w-20" />
+        <p className="px-4 my-auto">
+          Listening to <i>{lastFM.song.name}</i> by <i>{lastFM.song.artist}</i>
+        </p>
+      </div>
     </a>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      transitionProperty: {
-        height: "height",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

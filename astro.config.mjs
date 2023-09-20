48fa887a3c66c440d771c6a997f595e285d026ad
Jason Huang <jasonhuang20035@gmail.com>
2023-09-20 12:07:26 -0400

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({

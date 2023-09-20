141d64ffafd612a7d91a6822ce71a5aef5939a69
Jason Huang <jasonhuang20035@gmail.com>
2023-09-20 12:10:28 -0400

    i feel like sharp should work tho

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  image: {
-    service: squooshImageService(),
-  },
 });

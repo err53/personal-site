09ed7a2ae44b442fe7a40ef5bca79b02ab33d992
Jason Huang <jasonhuang20035@gmail.com>
2023-09-20 12:06:37 -0400

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,7 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: vercel({
-    analytics: true,
-    imageService: true,
-    imagesConfig: {
-      sizes: [398, 796, 1360],
-    },
-  }),
-  vite: {
-    define: {
-      "import.meta.env.VITE_VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-      "import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID": JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-    },
-  }
+  image: {
+    service: squooshImageService(),
+  },
 });

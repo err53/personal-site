ba327b78f13239fdc3e86943fa5d6a24df55d2fa
Jason Huang <jasonhuang20035@gmail.com>
2023-09-22 10:14:45 -0400

    add critters to inline css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import Critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), Critters()],
 });

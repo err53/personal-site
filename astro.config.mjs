3c70209c00bc22d35904917ef6fdfe2ad8af87bc
Jason Huang <jasonhuang20035@gmail.com>
2023-09-26 10:57:35 -0400

    Revert "add partytown"
    This reverts commit 20c88da81e48a40d659e25b47ab5e14e08205d94.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import Critters from "astro-critters";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), Critters(), partytown()]
+  integrations: [tailwind(), react(), Critters()],
 });

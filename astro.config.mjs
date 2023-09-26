20c88da81e48a40d659e25b47ab5e14e08205d94
Jason Huang <jasonhuang20035@gmail.com>
2023-09-26 10:54:31 -0400

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import Critters from "astro-critters";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), Critters()],
+  integrations: [tailwind(), react(), Critters(), partytown()]
 });

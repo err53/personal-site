410c97c92eb3bbb0d5aec438ea58c48e8d5b68d0
Jason Huang <jasonhuang20035@gmail.com>
2024-01-29 14:05:30 -0500

    fix image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lastfm.freetls.fastly.net",
+        port: "",
+        pathname: "/i/u/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

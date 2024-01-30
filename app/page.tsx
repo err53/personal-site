7ab00a70b9d599be3cd01b44ae1366c6e6405119
Jason Huang <jasonhuang20035@gmail.com>
2024-01-29 21:23:42 -0500

    disable prefetch to improve load speed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import LastFM from "./components/LastFM";
-import getLatestTrack from "./lib/getLatestTrack";
 import { Hero } from "./components/Hero";
 import { Socials } from "./components/Socials";
 import { Resume } from "./components/Resume";
@@ -13,13 +7,7 @@ import { Projects } from "./components/Projects";
 export const runtime = "edge";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
   const user = "err53";
-
-  await queryClient.prefetchQuery({
-    queryKey: ["user.getrecenttracks", user, "&limit=1"],
-    queryFn: getLatestTrack,
-  });
 
   return (
     <main className="container grid grid-cols-1 items-baseline gap-4 md:grid-cols-2">
@@ -27,9 +15,7 @@ export default async function Home() {
       <Socials />
       <Resume />
       <Projects />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <LastFM user={user} />
-      </HydrationBoundary>
+      <LastFM user={user} />
     </main>
   );
 }

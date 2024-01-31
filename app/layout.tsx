70ab3cb8ec5ad03cf26d1cd10b2ba6974050403b
Jason Huang <jasonhuang20035@gmail.com>
2024-01-31 10:36:21 -0500

    Revert "re-add cf analytics"
    This reverts commit 560918bd456d8537c8b993778c08840515fc1d71.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 import Providers from "./providers";
 
@@ -21,11 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "7909bf427b24443f8d47746dce69ad72"}'
-      />
       <body>
         <Providers>
           {children}

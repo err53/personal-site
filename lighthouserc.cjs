ef69ff3ee7648b4ef064b11b7c15f00c27ce77d0
Jason Huang <jasonhuang20035@gmail.com>
2024-01-31 13:32:18 -0500

    update lighthouse config file

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ["http://localhost:3000"],
-      startServerCommand: "NODE_ENV=production npm run start",
-    },
     upload: {
       target: "lhci",
       serverBaseUrl: "https://lhci.huang.ovh/",

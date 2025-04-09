8b4cc6bc5fdcc2fcef46c15634371eae9d2ba08c
Jason Huang <jasonhuang20035@gmail.com>
2025-04-08 23:13:36 -0400

    feat: add meetingbot

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -7,6 +7,7 @@ import dh_portal from "@/app/assets/dh_portal.png";
 import salesbop from "@/app/assets/salesbop.png";
 import cs_wiki from "@/app/assets/cs_wiki.png";
 import oscs from "@/app/assets/oscs.png";
+import meetingbot from "@/app/assets/meetingbot.png";
 
 const projectSizes = "(min-width: 640px) 50vw, 100vw";
 
@@ -14,14 +15,14 @@ export const Projects = () => (
   <div className="col-span-full">
     <h2 className="pb-3 text-3xl font-semibold">Projects</h2>
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      <Card href="https://github.com/err53/personal-site">
+      <Card href="https://meetingbot.tech/">
         <CardHeader>
-          <h3 className="text-2xl">Personal Site</h3>
-          <p>You&apos;re looking at it right now!</p>
+          <h3 className="text-2xl">meetingbot</h3>
+          <p>An open-source meeting bot API deployed on AWS</p>
         </CardHeader>
         <Image
-          src={personal_site}
-          alt="A screenshot of this website."
+          src={meetingbot}
+          alt="A screenshot of meetingbot."
           sizes={projectSizes}
           style={{
             width: "100%",
@@ -69,7 +70,10 @@ export const Projects = () => (
       <Card href="https://www.salesbop.io">
         <CardHeader>
           <h3 className="text-2xl">SalesBop</h3>
-          <p>A comprehensive, customizable sales coaching platform with natural language feedback</p>
+          <p>
+            A comprehensive, customizable sales coaching platform with natural
+            language feedback
+          </p>
         </CardHeader>
         <Image
           src={salesbop}
@@ -79,6 +83,24 @@ export const Projects = () => (
             width: "100%",
             height: "auto",
           }}
+        />
+      </Card>
+
+      <Card href="https://github.com/err53/personal-site">
+        <CardHeader>
+          <h3 className="text-2xl">Personal Site</h3>
+          <p>You&apos;re looking at it right now!</p>
+        </CardHeader>
+        <Image
+          src={personal_site}
+          alt="A screenshot of this website."
+          sizes={projectSizes}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          loading="eager"
+          priority
         />
       </Card>
 

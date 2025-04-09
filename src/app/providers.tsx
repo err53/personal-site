ef3f4e70856389a21b0a545f9a36e1c524afc712
Jason Huang <jasonhuang20035@gmail.com>
2025-04-09 15:22:10 -0400

    feat: track profiles for all users

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -15,7 +15,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
       api_host: "/ingest",
       ui_host: "https://us.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      person_profiles: "always", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false,
       capture_pageleave: true,
     });

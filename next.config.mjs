403a2b7461b3a4b936b1ff66173fbe160db407b9
Jason Huang <jasonhuang20035@gmail.com>
2025-09-05 13:48:53 -0400

    add image qualities

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ import "./src/env.js";
 /** @type {import('next').NextConfig} */
 const config = {
   images: {
+    qualities: [25, 50, 75],
     remotePatterns: [
       {
         protocol: "https",

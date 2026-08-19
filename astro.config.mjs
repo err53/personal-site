a0162136f996c87e0fd0e288c8a61d4e72e507b1
Jason Huang <jasonhuang20035@gmail.com>
2026-08-18 23:48:45 -0400

    everything except for lastfm

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
+    },
+    image: {
+        layout: "constrained"
     }
 });
